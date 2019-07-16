99feed8540fe3d9bbba27de4557b5b6ec8369614
Jonathan Forney <jonathan.forney@gmail.com>
2019-07-16 11:20:24 -0700

    partial server

---
+++ server.js
@@ -0,0 +1,11 @@
+const app = require('./lib/app');
+const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost:27017/human_habits');
+mongoose.connect();
+
+const PORT = ;
+
+app.listen(PORT), () => {
+
+}
